15b79cf73f321da58c21a83137285eeb382cf85f
Jack Pritchard <jack@noface.co.uk>
2020-01-03 14:03:42 +0000

    Lesson 4 - Global styles and theme values

--- components/particles/globalStyles.jsx
+++ components/particles/globalStyles.jsx
@@ -1,9 +1,318 @@
 import { createGlobalStyle } from "styled-components";
+import "normalize.css/normalize.css";
 
-const GlobalStyles = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
+		/* Accessibly remove animations: https://gist.githubusercontent.com/bellangerq/6cdfe6e3701b4048c72546960c7c9f66/raw/dc5036697d0da57eff8e0f659106b319102e72a0/a11y-disable-animations.css */
+	@media (prefers-reduced-motion: reduce) {
+		*,
+		*::before,
+		*::after {
+			animation-duration: 0.001ms !important;
+			animation-iteration-count: 1 !important;
+			transition-duration: 0.001ms !important;
+		}
+	}
+
+	html {
+		color: black;
+	  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Arial, Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+		font-size: 62.5%;
+		/* BETTER FONT SMOOTHING - https://gist.github.com/hsleonis/55712b0eafc9b25f1944 */
+		font-variant-ligatures: none;
+		-webkit-font-variant-ligatures: none;
+		text-rendering: optimizeLegibility;
+		-moz-osx-font-smoothing: grayscale;
+		font-smoothing: antialiased;
+		-webkit-font-smoothing: antialiased;
+		text-shadow: rgba(0, 0, 0, 0.01) 0 0 1px;
+	}
+
+	html {
+		box-sizing: border-box;
+	}
+
+	*, *:before, *:after {
+		box-sizing: inherit;
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	.link--icon::after {
+		display: none !important;
+	}
+
+	p {
+		a {
+			color: ${props => props.theme.primary};
+
+			&:active,
+			&:focus,
+			&:hover {
+				text-decoration: underline;
+			}
+		}
+	}
+
+	button {
+		font-weight: 700;
+	}
+
 	body {
+		overflow-y: scroll;
+
+		background-color: ${props => props.theme.white};
+		font-size: 1.6rem;
+		line-height: 1.5;
+		scroll-behavior: smooth;
+	}
+
+	main {
+		flex: 1;
+		margin: 0 auto;
+		width: 100%;
+
+		> * {
+			margin-left: auto;
+			margin-right: auto;
+			max-width: 1440px;
+		}
+	}
+
+	#___gatsby {
+		/* Fix anchor scroll positioning */
+		[id]::before {
+			display: block;
+			content: '';
+			margin-top: -4rem;
+			height: 4rem;
+		}
+	}
+
+	.wrapper {
+		overflow: hidden;
+
+		@supports (display: flex) {
+			display: flex;
+			flex-direction: column;
+			min-height: 100vh;
+		}
+	}
+
+	.grid {
+		margin: 0 auto;
+		max-width: ${props => props.theme.gridMax};
+		padding: 0 30px;
+	}
+
+	/* Common base styles for the site */
+	figure, img, svg, video {
+		max-width: 100%;
+	}
+
+	figure {
+		width: auto !important;
+	}
+
+	video {
+		display: block;
+		width: 100%;
+	}
+
+	button,
+	input[type="submit"] {
+		padding: 16px;
+
 		background-color: ${props => props.theme.primary};
+		border: none;
+		box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+		color: ${props => props.theme.white};
+		cursor: pointer;
+		font-weight: 700;
+		line-height: 1;
+		outline: none;
+		text-decoration: none;
+		transition: all 0.15s ease;
+		white-space: nowrap;
+	}
+
+
+	h1, h2, h3, h4, h5, h6 {
+		line-height: 1.25;
+		margin: 16px 0;
+
+		font-weight: 700;
+		text-transform: capitalize;
+	}
+
+	h1,
+	.h1 {
+		margin-bottom: 24px;
+		margin-top: 24px;
+
+		font-size: 32px;
+		font-weight: 700;
+		line-height: 1.1;
+	}
+
+	h2,
+	.h2 {
+		font-size: 30px;
+	}
+
+	h3,
+	.h3 {
+		font-size: 24px;
+	}
+
+	h4,
+	.h4 {
+		font-size: 20px;
+	}
+
+	h5,
+	.h5 {
+		font-size: 18px;
+	}
+
+	h6,
+	.h6 {
+		font-size: 16px;
+	}
+
+	main {
+		ol,
+		ul {
+			margin: 24px 0;
+		}
+	}
+
+	main article {
+		ol,
+		ul {
+			margin: 32px 0;
+		}
+
+		li {
+			font-size: 16px;
+			font-weight: 400;
+			line-height: 30px;
+
+			+ li {
+				margin-top: 24px;
+			}
+		}
+	}
+
+	ol {
+		padding-left: 24px;
+
+		counter-reset: list-counter;
+		list-style: none;
+	}
+
+	ol {
+		margin: 48px 0;
+
+		li {
+			position: relative;
+
+			counter-increment: list-counter;
+			font-size: 16px;
+			line-height: 30px;
+
+			+ li {
+				margin-top: 12px;
+			}
+
+			&::before {
+				position: absolute;
+				top: 0;
+				left: 0;
+
+				color: ${props => props.theme.primary};
+				content: counter(list-counter) ". ";
+				font-style: normal;
+				font-weight: bold;
+
+				transform: translateX(-110%);
+				transform: translateX(calc(-100% - 12px));
+			}
+		}
+	}
+
+	/* FORM */
+	button[type="submit"] {
+		margin-top: 24px;
+	}
+
+	input[disabled] {
+		cursor: not-allowed;
+	}
+
+  input,
+  label,
+  textarea {
+    display: block;
+    width: 100%;
+	}
+
+  input,
+  textarea {
+    padding: 16px;
+
+		background-color: ${props => props.theme.white};
+		border: 1px solid rgba(0, 0, 0, 0.12);
+
+    & + & {
+      margin-top: 24px;
+    }
+  }
+
+  label {
+    margin: 16px 0 8px;
+
+		color: ${props => props.theme.black};
+		font-size: 16px;
+		font-weight: bold;
+		line-height: 32px;
+    text-transform: capitalize;
+	}
+
+	textarea {
+		min-height: 150px;
+		resize: vertical;
+		width: 100%;
+	}
+
+	option {
+		padding: 16px;
+
+		font-weight:normal;
+	}
+
+	::placeholder {
+		color: ${props => props.theme.grey500};
+		font-size: 14px;
+		font-weight: 300;
+		line-height: 20px;
+	}
+
+	/* https://www.scottohara.me/blog/2017/04/14/inclusively-hidden.html */
+	.hide:not(:focus):not(:active),
+	.hidden:not(:focus):not(:active) {
+		clip: rect(0 0 0 0);
+		clip-path: inset(50%);
+		height: 1px;
+		overflow: hidden;
+		position: absolute;
+		white-space: nowrap;
+		width: 1px;
+		}
 	}
 `;
 
-export default GlobalStyles;
+export default GlobalStyle;
--- components/particles/themeDefault.jsx
+++ components/particles/themeDefault.jsx
@@ -1,5 +1,40 @@
 const themeDefault = {
-	primary: "rebeccapurple"
+	gridMax: "1440px",
+	/* Colours */
+	white: "#ffffff",
+	offWhite: "#F0F0F0",
+	grey: "#a6a6a6",
+	grey100: "#d9d9d9",
+	grey200: "#bfbfbf",
+	grey300: "#a6a6a6",
+	grey400: "#8c8c8c",
+	grey500: "#737373",
+	grey600: "#595959",
+	grey700: "#404040",
+	grey800: "#262626",
+	grey900: "#0d0d0d",
+	black: "#141213",
+	purple100: "#EDE9FB",
+	purple200: "#C9BEF3",
+	purple300: "#A593EC",
+	purple400: "#8168E4",
+	purple500: "#5D3DDC",
+	purple600: "#4E2ECD",
+	purple700: "#341B97",
+	purple800: "#25136C",
+	purple900: "#170C41",
+	primary: "#5D3DDC",
+	primary100: "#EDE9FB",
+	primary200: "#C9BEF3",
+	primary300: "#A593EC",
+	primary400: "#8168E4",
+	primary500: "#5D3DDC",
+	primary600: "#4E2ECD",
+	primary700: "#341B97",
+	primary800: "#25136C",
+	primary900: "#170C41",
+	secondary: "#3788A1",
+	tertiary: "#2ECDBA"
 };
 
-export default themeDefault;
+module.exports = themeDefault;
