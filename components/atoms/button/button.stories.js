c4838444ade46703f3da2d8496f3c8bccb81bd10
Jack Pritchard <jack@noface.co.uk>
2020-01-03 13:37:54 +0000

    Lesson 3 - Update story to include basic button

--- components/atoms/button/button.stories.js
+++ components/atoms/button/button.stories.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { action } from "@storybook/addon-actions";
-
 import Button from "./button";
 
-export const text = () => (
-	<Button onClick={action("clicked")}>Just a Button</Button>
-);
-
-export const emoji = () => (
-	<Button onClick={action("clicked")}>
-		<span role="img" aria-label="so cool">
-			😀 😎 👍 💯
-		</span>
-	</Button>
-);
+export const basicButton = () => <Button>Basic button</Button>;
 
 export default {
 	component: Button,
